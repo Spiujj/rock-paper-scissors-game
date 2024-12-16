9bcca865d748ab9153a3d99c15d947011f0cc45c
Onur Bilgi <onurbilgi2010@gmail.com>
2024-12-16 13:33:36 +0300

    Update rock paper scissors game.py

--- rock paper scissors game.py
+++ rock paper scissors game.py
@@ -1,50 +1,57 @@
 import random
+import keyboard
 
 ifadeler=["Taş","Kağıt","Makas"]
 ifadeseçme=random.choice(ifadeler)
 
- 
+print("Başlamak için Y basınız")
+
+def main(): 
+    while True:
+            oyuncu_ifadesi=input("Taş, Kağıt veya Makas seçiniz: ")
+            print("Yapay Zeka Tarafından Seçilen İfade:", ifadeseçme)
+                
+
+            if ifadeseçme=="Taş" and oyuncu_ifadesi=="Taş":
+                print("Berabere!")
+            
+
+
+            if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Kağıt":
+                print("Berabere!") 
+                
+
+            if ifadeseçme=="Makas" and oyuncu_ifadesi=="Makas":
+                print("Berabere!")   
+                
+
+            if ifadeseçme=="Taş" and oyuncu_ifadesi=="Kağıt"  :
+                print("Yapay Zeka Yendi")
+                
+
+            if ifadeseçme=="Taş" and oyuncu_ifadesi=="Makas":
+                print("Oyuncu Yendi") 
+                
+
+            if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Taş":
+                print("Yapaz Zeka Yendi")  
+                
+
+            if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Makas":
+                print("Oyuncu yendi")  
+                
+
+            if ifadeseçme=="Makas" and oyuncu_ifadesi=="Taş":
+                print("Oyuncu Yendi")    
+            
+                
+            if ifadeseçme=="Makas" and oyuncu_ifadesi=="Kağıt":
+                print("Yapaz Zeka Yendi")
+                
+            else:
+                print("HATA baş harf büyük olcak şekilde yazınız")  #
 while True:
-        oyuncu_ifadesi=input("Taş, Kağıt veya Makas girin: ")
-        print("Yapay Zeka Tarafından Seçilen İfade:", ifadeseçme)
-            
-
-        if ifadeseçme=="Taş" and oyuncu_ifadesi=="Taş":
-            print("Berabere!")
-           
-
-
-        if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Kağıt":
-            print("Berabere!") 
-             
-
-        if ifadeseçme=="Makas" and oyuncu_ifadesi=="Makas":
-            print("Berabere!")   
-            
-
-        if ifadeseçme=="Taş" and oyuncu_ifadesi=="Kağıt"  :
-            print("Yapay Zeka Yendi")
-            
-
-        if ifadeseçme=="Taş" and oyuncu_ifadesi=="Makas":
-            print("Oyuncu Yendi") 
-              
-
-        if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Taş":
-            print("Yapaz Zeka Yendi")  
-             
-
-        if ifadeseçme=="Kağıt" and oyuncu_ifadesi=="Makas":
-            print("Oyuncu yendi")  
-             
-
-        if ifadeseçme=="Makas" and oyuncu_ifadesi=="Taş":
-            print("Oyuncu Yendi")    
-           
-            
-        if ifadeseçme=="Makas" and oyuncu_ifadesi=="Kağıt":
-            print("Yapaz Zeka Yendi")
-             
-        
-
+    event = keyboard.read_event(suppress=True)  
+    if event.name == 'y' and event.event_type == 'down':  
+        main()   
         
